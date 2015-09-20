3a6a0451a42989ced9a7ad9c419a007c3b6b910e
Victor Frandsen <mrweektor@gmail.com>
2015-09-20 03:51:21 -0700

    increased timeview textSize

--- app/src/main/java/se/frand/app/timer/MainActivity.java
+++ app/src/main/java/se/frand/app/timer/MainActivity.java
@@ -4,12 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -20,13 +21,45 @@ public class MainActivity extends Activity {
     TimerTask timerTask;
     long time;
     Calendar calendar;
+    Button startButton;
+    Button stopButton;
+    TextView timeView;
+    LinearLayout layout;
+
+
 
     final Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        timeView = new TextView(this);
+        timeView.setTextSize(30.0f);
 
+        startButton = new Button(this);
+        startButton.setText("start");
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer();
+            }
+        });
+
+        stopButton = new Button(this);
+        stopButton.setText("stop");
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                stopTimerTask();
+            }
+        });
+
+        layout.addView(timeView);
+        layout.addView(startButton);
+
+        setContentView(layout);
 
         time = 0;
     }
@@ -38,24 +71,29 @@ public class MainActivity extends Activity {
         //startTimer();
     }
 
-    public void startTimer(View v) {
+    private void startTimer() {
         timer = new Timer();
 
         initializeTimerTask();
         timer.schedule(timerTask, 0, 1000);
+
+        layout.removeView(startButton);
+        layout.addView(stopButton);
     }
 
-    private void stopTimerTask(View v) {
+    private void stopTimerTask() {
+        time = 0;
         if(timer != null) {
             timer.cancel();
             timer = null;
         }
+        layout.removeView(stopButton);
+        layout.addView(startButton);
     }
 
     private void initializeTimerTask() {
 
         calendar = Calendar.getInstance();
-        calendar.setTimeZone(TimeZone.);
 
 
         timerTask = new TimerTask() {
@@ -65,11 +103,11 @@ public class MainActivity extends Activity {
                     @Override
                     public void run() {
                         calendar.setTime(new Date(time*1000L));
-                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("K:mm:ss");
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("mm:ss");
                         final String strDate = simpleDateFormat.format(calendar.getTime());
-                        TextView view = (TextView) findViewById(R.id.time_display_text);
 
-                        view.setText(strDate);
+
+                        timeView.setText(strDate);
 
                         time+=1;
                     }
